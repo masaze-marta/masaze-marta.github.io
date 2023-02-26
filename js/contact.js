43029d9ff0476e595139b69a24c41fbfd9ae97f8
jba <jan.bartosek@artisys.aero>
2023-02-26 19:53:02 +0100

    tons of changes

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,10 @@
 function send() {
     let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
     let phone = document.getElementById("phone").value;
     let body = document.getElementById("body").value;
-    window.open(`mailto:hanakovamarta@email.cz?subject=${subject}&body=${body}`);
+
+    let text = `Jméno: ${name}%0D%0ATelefon: ${phone}%0D%0A%0D%0A${body.replaceAll("\n", "%0D%0A")}`;
+
+    window.open(`mailto:hanakovamarta@email.cz?subject=${subject}&body=${text}`);
 }
